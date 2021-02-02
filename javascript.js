cd407e57e5b1e027d2de51210cc397f6f8c89c29
sophie-war-hier <74067483+sophie-war-hier@users.noreply.github.com>
2021-02-02 09:18:17 +0100

    verbesserung und hinzufügen von js

---
+++ javascript.js
@@ -0,0 +1,72 @@
+	
+function next0() {
+	document.getElementById("img").src = "bilder/druck"+0+".png";
+}
+
+function next1() {
+	document.getElementById("img").src = "bilder/druck"+1+".png";
+} 
+
+function next2() {
+	document.getElementById("img").src = "bilder/druck"+2+".png";
+}
+
+function next3() {
+	document.getElementById("img").src = "bilder/druck"+4+".png";
+
+}
+
+function next4() {
+	document.getElementById("img").src = "bilder/druck"+5+".png";
+}
+
+
+function next5() {
+	document.getElementById("img").src = "bilder/druck"+7+".png";
+}
+
+
+function next6() {
+	document.getElementById("img").src = "bilder/druck"+10+".png";
+}
+
+function next7() {
+	document.getElementById("img").src = "bilder/druck"+11+".png";
+}
+
+function next8() {
+	document.getElementById("img").src = "bilder/druck"+13+".png";
+}
+
+function next9() {
+	document.getElementById("img").src = "bilder/druck"+14+".png";
+}
+
+function next10() {
+	document.getElementById("img").src = "bilder/druck"+15+".png";
+}
+
+function next11() {
+	document.getElementById("img").src = "bilder/druck"+16+".png";
+}
+
+function next12() {
+	document.getElementById("img").src = "bilder/druck"+19+".png";
+}
+
+function next13() {
+	document.getElementById("img").src = "bilder/druck"+21+".png";
+}
+
+function next14() {
+	document.getElementById("img").src = "bilder/druck"+22+".png";
+}
+
+function next15() {
+	document.getElementById("img").src = "bilder/druck"+23+".png";
+}
+
+
+
+
+
